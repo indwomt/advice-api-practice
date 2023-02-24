088513a4dfb8f6a6a3076c5d4075f450d23654ea
Lauren Bentley <laurendebentley@gmail.com>
2023-02-24 17:57:24 -0500

    need to make button functional and edit fonts/colors

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('hello')
+
+let adviceURL = 'https://api.adviceslip.com/advice'
+let adviceContent = $('.advice-content')
+let adviceNumber = $('.advice-number')
+let button = $('.next-button')
+
+function getAdvice() {
+    fetch(adviceURL)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data)
+        adviceNumber.text('Advice #' + data.slip.id )
+        adviceContent.text(data.slip.advice)
+    })
+    
+
+
+}
+
+button.addEventListener('click', getAdvice())
+
+getAdvice()
